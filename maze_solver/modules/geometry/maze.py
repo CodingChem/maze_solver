b4b0c175f210dd4a3cfd11c9797bbee0fde2d62c
Vegard Seines <vegsei@gmail.com>
2024-11-27 14:40:54 +0100

    fix: Bugfixing in maze class
    Updated redraw method not to be stuck in infinite loop
    added break wall method to maze class

--- maze_solver/modules/geometry/maze.py
+++ maze_solver/modules/geometry/maze.py
@@ -1,7 +1,9 @@
 from time import sleep
 from .cell import Cell
+from .cell_wall import WallType
 from .window import Window
 from .point import Point
+import random
 
 
 class Maze:
@@ -14,7 +16,10 @@ class Maze:
         num_rows: int,
         num_cols: int,
         win: Window,
+        seed: int | None = None,
     ) -> None:
+        if seed is not None:
+            random.seed(seed)
         self.cell_width = width // num_cols
         self.cell_height = height // num_rows
         self.x = x
@@ -51,10 +56,47 @@ class Maze:
                 cel.draw()
 
     def _animate(self):
-        while True:
-            self._win.redraw()
-            sleep(0.05)
+        self._win.redraw()
+        sleep(0.05)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_left_wall = False
         self._cells[-1][-1].has_right_wall = False
+
+    def _break_walls_r(self, i, j):
+        current_cell = self._cells[i][j]
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            for neighbor in [
+                x for x in self._get_adjecant_cells(i, j) if not x.visited
+            ]:
+                to_visit.append(neighbor)
+            if len(to_visit) < 1:
+                return
+            target = random.choice(to_visit)
+            current_cell.break_walls(target)
+            target_i, target_j = self._get_cell_coordinates(target)
+            self._break_walls_r(target_i, target_j)
+
+    def _get_cell_coordinates(self, cell: Cell) -> tuple[int, int]:
+        for i in range(len(self._cells)):
+            if cell not in self._cells[i]:
+                continue
+            j = self._cells[i].index(cell)
+            return i, j
+        raise Exception("Cell not in grid!")
+
+    def _get_adjecant_cells(self, i, j) -> list[Cell]:
+        adjecant = []
+        for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            try:
+                adjecant.append(self._cells[i - x][j - y])
+            except IndexError:
+                continue
+        return adjecant
+
+    def _reset_cells_visited(self):
+        for line in self._cells:
+            for cell in line:
+                cell.visited = False
