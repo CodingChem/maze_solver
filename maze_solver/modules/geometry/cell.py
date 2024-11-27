525ed28871fb69dfe4914b9e75f01c60bd05ec52
Vegard Seines <vegsei@gmail.com>
2024-11-27 10:31:04 +0100

    fix cell class
    The cell class now initializes and can be edited and moved correctly. it
    also correctly draws its walls now.

--- maze_solver/modules/geometry/cell.py
+++ maze_solver/modules/geometry/cell.py
@@ -16,19 +16,15 @@ class Cell:
         has_top_wall: bool = True,
         has_bottom_wall: bool = True,
     ):
-        self.walls = {
-            WallType.LEFT: None,
-            WallType.RIGHT: None,
-            WallType.TOP: None,
-            WallType.BOTTOM: None,
-        }
+        self.walls = {}
         self.set_pos(upper_left, bottom_right)
         self.set_walls(has_left_wall, has_right_wall, has_top_wall, has_bottom_wall)
         self._win = win
+        self.visited = False
 
     def draw(self) -> None:
-        for line in self.get_walls():
-            line[0].draw(self._win.canvas, line[1])
+        for wall in self.get_walls():
+            wall.draw(self._win.canvas, None)
 
     def set_walls(self, lw: bool, rw: bool, tw: bool, bw: bool):
         self.has_left_wall = lw
@@ -41,6 +37,18 @@ class Cell:
         self.bottom_right = bottom_right
         self.upper_right = Point(bottom_right.x, upper_left.y)
         self.bottom_left = Point(upper_left.x, bottom_right.y)
+        self.walls[WallType.TOP] = CellWall(
+            self.upper_left, self.upper_right, self.has_top_wall
+        )
+        self.walls[WallType.BOTTOM] = CellWall(
+            self.bottom_left, self.bottom_right, self.has_bottom_wall
+        )
+        self.walls[WallType.LEFT] = CellWall(
+            self.upper_left, self.bottom_left, self.has_left_wall
+        )
+        self.walls[WallType.RIGHT] = CellWall(
+            self.upper_right, self.bottom_right, self.has_right_wall
+        )
 
     def translate_cell(self, x_off: int, y_off: int):
         new_upper_left = Point(self.upper_left.x + x_off, self.upper_left.y + y_off)
@@ -55,8 +63,8 @@ class Cell:
             self.has_bottom_wall,
         )
 
-    def get_walls(self) -> list[tuple[Line, str]]:
-        return [x for x in self.walls.values() if x is not None]
+    def get_walls(self) -> list[CellWall]:
+        return [x for x in self.walls.values()]
 
     def get_center(self) -> Point:
         return Point(
@@ -73,4 +81,40 @@ class Cell:
 
     @property
     def has_left_wall(self):
+        if WallType.LEFT not in self.walls:
+            return False
         return self.walls[WallType.LEFT].iswall()
+
+    @has_left_wall.setter
+    def has_left_wall(self, arg: bool):
+        self.walls[WallType.LEFT].exists = arg
+
+    @property
+    def has_right_wall(self):
+        if WallType.RIGHT not in self.walls:
+            return False
+        return self.walls[WallType.RIGHT].iswall()
+
+    @has_right_wall.setter
+    def has_right_wall(self, arg: bool):
+        self.walls[WallType.RIGHT].exists = arg
+
+    @property
+    def has_top_wall(self):
+        if WallType.TOP not in self.walls:
+            return False
+        return self.walls[WallType.TOP].iswall()
+
+    @has_top_wall.setter
+    def has_top_wall(self, arg: bool):
+        self.walls[WallType.TOP].exists = arg
+
+    @property
+    def has_bottom_wall(self):
+        if WallType.BOTTOM not in self.walls:
+            return False
+        return self.walls[WallType.BOTTOM].iswall()
+
+    @has_bottom_wall.setter
+    def has_bottom_wall(self, arg: bool):
+        self.walls[WallType.BOTTOM].exists = arg
